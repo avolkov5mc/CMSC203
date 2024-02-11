8b872abb9c760ad72863545b97528cd52f05b8a7
JLeopolt <alexsib.v@gmail.com>
2024-02-11 11:55:06 -0500

    WIP

--- src/main/java/ESPGame.java
+++ src/main/java/ESPGame.java
@@ -1,2 +1,87 @@
-public class Assignment1 {
+/*
+* Class: CMSC203 CRN 30339
+* Instructor: Grigoriy Grinberg
+* Description: An ESP training game
+* Due: 2/12/24
+* Platform/compiler: Java/IntelliJ
+* I pledge that I have completed the programming assignment
+* independently. I have not copied the code from a student or
+* any source. I have not given my code to any student.
+* Print your Name here: Alexei Volkov
+*/
+import java.util.Scanner;
+public class ESPGame {
+
+    private static final String COLOR_RED = "Red";
+    private static final String COLOR_GREEN = "Green";
+    private static final String COLOR_BLUE = "Blue";
+    private static final String COLOR_ORANGE = "Orange";
+    private static final String COLOR_YELLOW = "Yellow";
+
+    public static void main(String[] args){
+        // Ask user for information about themselves
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = keyboard.nextLine();
+        System.out.println("Describe yourself: ");
+        String description = keyboard.nextLine();
+        System.out.println("Due Date: ");
+        String dueDate = keyboard.nextLine();
+
+        System.out.println("CMSC203 Assignment #1: Test your ESP skills!");
+        int roundsWon = 0;
+
+        for(int i = 0; i < 11; i++){
+            System.out.println("Round " + i);
+            // pick a random number 1-5
+            int computerChoice = (int) (Math.random() * 6) + 1;
+            System.out.println("I am thinking of a number.");
+            System.out.println("Is it Red, Green, Blue, Orange, or Yellow?");
+            System.out.println("Enter your guess: ");
+            int userChoice;
+            // prompt user for a color until they enter a valid one
+            do{
+                String userColor = keyboard.nextLine();
+                switch(userColor){
+                    case(COLOR_RED):
+                        userChoice = 1;
+                        break;
+                    case(COLOR_GREEN):
+                        userChoice = 2;
+                        break;
+                    case(COLOR_BLUE):
+                        userChoice = 3;
+                        break;
+                    case(COLOR_ORANGE):
+                        userChoice = 4;
+                        break;
+                    case(COLOR_YELLOW):
+                        userChoice = 5;
+                        break;
+                    default:
+                        userChoice = -1;
+                        System.out.println("You entered an incorrect color. Is it Red, Green, Blue, Orange, or Yellow? ");
+                }
+            }while(userChoice != -1);
+
+            String computerColor;
+            switch(computerChoice){
+                case(1):
+                    computerColor = COLOR_RED;
+                    break;
+                case(2):
+                    computerColor = COLOR_GREEN;
+                    break;
+                case(3):
+                    computerColor = COLOR_BLUE;
+                    break;
+                case(4):
+                    computerColor = COLOR_ORANGE;
+                    break;
+                case(5):
+                    computerColor = COLOR_YELLOW;
+                    break;
+            }
+        }
+    }
 }
